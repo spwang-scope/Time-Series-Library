17c6ec7cf45666041a086c39f45456fb20afb20f
spwang <spwang@mail.com>
2025-09-04 00:59:12 +0800

    disable all other specified datasets

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -1,22 +1,10 @@
-from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_M4, PSMSegLoader, \
-    MSLSegLoader, SMAPSegLoader, SMDSegLoader, SWATSegLoader, UEAloader
+from data_provider.data_loader import Dataset_Custom
 from data_provider.uea import collate_fn
 from typing import Optional
 from torch.utils.data import DataLoader
 
 data_dict = {
-    'ETTh1': Dataset_ETT_hour,
-    'ETTh2': Dataset_ETT_hour,
-    'ETTm1': Dataset_ETT_minute,
-    'ETTm2': Dataset_ETT_minute,
     'custom': Dataset_Custom,
-    'm4': Dataset_M4,
-    'PSM': PSMSegLoader,
-    'MSL': MSLSegLoader,
-    'SMAP': SMAPSegLoader,
-    'SMD': SMDSegLoader,
-    'SWAT': SWATSegLoader,
-    'UEA': UEAloader
 }
 
 
